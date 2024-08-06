86b2db5723857a19c13b76c2dacfbccc8b717816
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-06 18:14:23 +0700

    feat: google font and custom color

--- src/components/login/login-header.tsx
+++ src/components/login/login-header.tsx
@@ -9,7 +9,7 @@ export const LoginHeader:React.FC<props> = (props) => {
   return (
     <div className='w-full text-center'>
         <h1 className='text-3xl font-semibold'>{props.heading}</h1>
-        <p className='text-xl font-semibold'>{props.subHeading}</p>
+        <p className='text-xl font-medium'>{props.subHeading}</p>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        customPurple: '#7F56D8', 
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",

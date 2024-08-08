a4695988491643af8666f42838ccd7ccec4c1eb1
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-08 10:16:03 +0700

    feat: header new ticket

--- src/pages/dashboard/new-ticket/index.tsx
+++ src/pages/dashboard/new-ticket/index.tsx
@@ -1,10 +1,19 @@
+import { NewTicketForm } from '@/components/@pagecomponents/new-ticket/new-ticket-form';
 import { DashboardLayout } from '@/components/layout/dashboard-layout';
+import { Separator } from '@/components/ui/separator';
 import React from 'react';
 
 const index = () => {
   return (
     <DashboardLayout title="New-Ticket">
-      <h1>test</h1>
+      <div className="flex flex-col space-y-2">
+        <h1 className="font-semibold text-lg">Create Quick Ticket</h1>
+        <p className="text-slate-300">
+          Write and address new queries and issues
+        </p>
+        <Separator className='w-full'/>
+        <NewTicketForm />
+      </div>
     </DashboardLayout>
   );
 };

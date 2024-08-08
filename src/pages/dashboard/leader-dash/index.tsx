6700171d25296f06f9b0eced1271cbb2d13c24db
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-08 19:58:15 +0700

    feat: leader dashboard wrap

--- src/pages/dashboard/leader-dash/index.tsx
+++ src/pages/dashboard/leader-dash/index.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 import profileBanner from '../../../assets/bannerprofile.svg';
 import avatar from '../../../assets/leaddash-profile.svg';
 import TicketCard from '@/components/ui/ticket-card';
-import newticketicon from '../../../assets/new-ticket.svg';
+import allticketicon from '../../../assets/all-tickets-dashboard-icon.svg';
+import newticketicon from '../../../assets/new-ticket-dashboard-icon.svg';
+import onprogressicon from '../../../assets/on-progress-dashboard-icon.svg';
+import cancelledicon from '../../../assets/cancelled-dashboard-icon.svg';
+import doneicon from '../../../assets/done-dashboard-icon.svg';
+import closedicon from '../../../assets/closed-dashboard-icon.svg';
+import messagesicon from '../../../assets/messages-dashboard-icon.svg';
+import reporticon from '../../../assets/report-dashboard-icon.svg';
 
 const index = () => {
   return (
@@ -14,18 +21,18 @@ const index = () => {
           banner={profileBanner}
           id="1"
           avatar={avatar}
-          position="Fullstack Developer"
-          username="Yanuar Prayoga"
+          position="Senior Project Manager"
+          username="Trias Bratakusuma"
         />
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-12 mx-4">
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
-          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={allticketicon} title="All Tickets" count="230" />
+          <TicketCard icon={newticketicon} title="New Tickets" count="12" />
+          <TicketCard icon={onprogressicon} title="On Progress" count="35" />
+          <TicketCard icon={cancelledicon} title="Cancelled" count="4" />
+          <TicketCard icon={doneicon} title="Done" count="27" />
+          <TicketCard icon={closedicon} title="Closed" count="153" />
+          <TicketCard icon={messagesicon} title="Messages" count="74" />
+          <TicketCard icon={reporticon} title="Report" count="38" />
         </div>
       </DashboardLayout>
     </>

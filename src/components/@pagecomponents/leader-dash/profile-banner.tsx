0f44644cc14d05eb16ecb9c625efd06d3bccd8e3
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-08 10:16:28 +0700

    feat: header new ticket

--- src/components/@pagecomponents/leader-dash/profile-banner.tsx
+++ src/components/@pagecomponents/leader-dash/profile-banner.tsx
@@ -20,7 +20,7 @@ export const ProfileBanner: React.FC<Props> = ({
   return (
     <div className="w-full h-1/3">
       <div className="h-full w-full relative">
-        <div className="h-[16rem] w-full bg-black opacity-40 absolute z-10"></div>
+        <div className="h-[16rem] w-full bg-black opacity-40 absolute"></div>
         <Image
           src={banner}
           alt="banner"
@@ -33,7 +33,7 @@ export const ProfileBanner: React.FC<Props> = ({
             <Image
               src={avatar}
               alt="avatar"
-              className="object-cover z-30 rounded-full h-[130px] bg-red-200 w-[130px] absolute left-4 -top-12"
+              className="object-cover rounded-full h-[130px] bg-red-200 w-[130px] absolute left-4 -top-12"
             />
             <div className="ml-40">
               <p className="font-semibold">{username}</p>

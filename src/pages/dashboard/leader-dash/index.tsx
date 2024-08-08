d2f076ce67f1e604a1fc9387e0083f961a54db46
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-08 19:41:40 +0700

    feat: slicing ticket card

--- src/pages/dashboard/leader-dash/index.tsx
+++ src/pages/dashboard/leader-dash/index.tsx
@@ -3,11 +3,32 @@ import { DashboardLayout } from '@/components/layout/dashboard-layout';
 import React from 'react';
 import profileBanner from '../../../assets/bannerprofile.svg';
 import avatar from '../../../assets/leaddash-profile.svg';
+import TicketCard from '@/components/ui/ticket-card';
+import newticketicon from '../../../assets/new-ticket.svg';
+
 const index = () => {
   return (
-    <DashboardLayout title="Leader Dashboard">
-      <ProfileBanner banner={profileBanner} id="1" avatar={avatar} position="Fullstack Developer" username="Yanuar Prayoga" />
-    </DashboardLayout>
+    <>
+      <DashboardLayout title="Leader Dashboard">
+        <ProfileBanner
+          banner={profileBanner}
+          id="1"
+          avatar={avatar}
+          position="Fullstack Developer"
+          username="Yanuar Prayoga"
+        />
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-12 mx-4">
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+          <TicketCard icon={newticketicon} title="All Tickets" count="20" />
+        </div>
+      </DashboardLayout>
+    </>
   );
 };
 
